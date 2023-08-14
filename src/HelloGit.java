2d5770f75520b2bd4260b9b11d8de2fa2020e702
Kevin <williepete230@outlook.com>
2023-08-14 14:36:59 -0400

    initial commit

---
+++ src/HelloGit.java
@@ -0,0 +1,9 @@
+/**
+ * Simple hello world program
+ * Author: Kevin Mullins
+ */
+public class HelloGit{
+    public static void main(String []args){
+        System.out.println("Hello World");
+    }
+}
